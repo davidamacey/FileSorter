3ca3dfb053f1f69f7b7b91abe0457e68a5d2a7f2
davidamacey <davidamacey@gmail.com>
2024-12-06 19:05:54 -0500

    updated date processing, added iphone instructions

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Media File Sorter
 
-## Description
-
 The Media File Sorter is a Python script that organizes media files (e.g., photos and videos) based on their creation date. It can be especially useful for managing large collections of media files that lack proper organization. The script can process multiple files concurrently to improve efficiency.
+
+Using this script, speed depends on hardware.  Using 24 cores and a local NAS, processing 18000 photos and videos totalling about 175 GB completed in about 8 minutes.
 
 ## Prerequisites
 
 Before using the script, ensure that you have the following dependencies installed:
 
-- Python 3.10
+- Python 3.11
 - The `exiftool` library
 
 You can install the required Python libraries using `pip`:
@@ -16,6 +16,27 @@ You can install the required Python libraries using `pip`:
 ```bash
 pip install -r requirements.txt
 ```
+### Download iPhone Photos to NAS
+
+Purpose is to copy files from iPhone device directly to the local NAS file storage
+
+## Connect iPhone
+
+Connect iPhone via USB connection and login via Ubuntu
+
+1. Turn off WiFi and Bluetooth, turn on Personal Hotspot
+2. Navigate to the network section of Files.
+3. Remove the `:3/` from the address to connect to iPhone system.  
+
+## Find Files to Copy
+
+Options:
+1. Manually go through iPhone DCIM folder and NAS Picture files and find latest file by date and select items to copy.
+2. Use a script (to be created) to find the latest file in NAS to select newer files on iPhone.
+
+Copy files to an import folder on the NAS:
+
+Select the DCIM folder and select the folders you would like to copy, then drop and drop to copy.
 
 ## Workflow
 
@@ -26,26 +47,22 @@ Intent to is to run each script individually with human in the loop.
 3. Run 'remove_dupes.py' script to remove the duplicate files within each sub folder given a directory.
 4. Run 'rsync' command to merge new media to master media directory, 'rsync -avh --progress --ignore-existing /source/folder/ /desitnation/folder'
 
-## Usage
+## Usage - Sorting by YYYY-MM-DD
 
 To sort your media files, run the script as follows:
 
+Using the year month day sorting method, run the script: `sort_img3.py` to move the files accordingly.
+
+**IMPORTANT NOTE:** This will by default move files and remove from source during copy process.  Reduces duplicate files and consolidates to single photo directory.
+
+Run Script:
 ```bash
-python sort_img.py source_dir dest_dir num_threads log_file
+time python sort_img.py /mnt/nas/import_pictures/ /mnt/nas/Pictures/ ./iphone_transfer_<date>.txt
 ```
 
 - `source_dir`: The source directory containing your unsorted media files.
 - `dest_dir`: The destination directory where the sorted files will be placed.
-- `num_threads`: The number of threads for concurrent processing (adjust this based on your system's capabilities).
 - `log_file`: The output log file to record the sorting process and statistics.
-
-## Example
-
-Sorting media files in `/mnt/nas/unsorted_photos_to_import/` into `/mnt/nas/TEST_SORT_OUTPUT/` using 47 threads:
-
-```bash
-python sort_img.py /mnt/nas/unsorted_photos_to_import/ /mnt/nas/TEST_SORT_OUTPUT/ 47 /mnt/nas/test_sort_output.txt
-```
 
 ## Removing `.DS_Store` Files
 
@@ -63,25 +80,19 @@ python remove_ds_store.py directory
 
 - `directory`: The directory in which you want to remove `.DS_Store` files.
 
-### Example
-
-Remove `.DS_Store` files from a directory:
-
-```bash
-python remove_ds_store.py /mnt/nas/your_directory/
-```
-
 ## Removing Duplicate Files
 
 ### Description
 
 The Duplicate File Remover is a Python script designed to find and remove duplicate files within a directory. It uses the SHA-256 hash of files to identify duplicates and provides the option to perform a dry run, allowing you to see what would be removed without actually deleting files.
 
+Use Case: Folders with duplicate files, but with different file names such as IMG_0001.JPG and IMG_0001-1.JPG.  Checks for duplicate hash, if so removes file.
+
 ### Prerequisites
 
 Before using the script, ensure that you have the following dependencies installed:
 
-- Python 3.10
+- Python 3.11
 
 ### Usage
 
@@ -94,36 +105,13 @@ python remove_dupes.py directory [--dry-run]
 - `directory`: The directory in which you want to find and remove duplicate files.
 - `--dry-run` (optional): Print what would be removed without deleting files. Use this flag for a trial run.
 
-### Example
-
-Remove duplicate files from a directory:
-
-```bash
-python remove_dupes.py /mnt/nas/your_directory/
-```
-
-Remove duplicate files from a directory with a dry run:
-
-```bash
-python remove_dupes.py /mnt/nas/your_directory/ --dry-run
-```
 
 ## Synchronizing Folders
 
 ### Description
 
-The folder synchronization script is a Bash script that uses the `rsync` command to synchronize files from a source folder to a destination folder. It can be used to keep two directories in sync efficiently. Please make sure you have `rsync` installed on your system.
-
-### Usage
-
-1. Edit the script to set your source folder, destination folder, and the number of CPU cores to utilize for parallel synchronization.
-
-2. Run the script:
+The folder synchronization via CLI that uses the `rsync` command to synchronize files from a source folder to a destination folder. It can be used to keep two directories in sync efficiently. Please make sure you have `rsync` installed on your system.
 
 ```bash
-./sync_folders.sh
+rsync -avh --progress --ignore-existing /source/folder/ /desitnation/folder'
 ```
-
-### Example
-
-Edit the script to specify your source and destination folders, and the number of CPU cores, and then run the script.
--- sort_img.py
+++ sort_img.py
@@ -1,49 +1,80 @@
-from os import makedirs, path, walk
+from os import makedirs, path, walk, remove
 from shutil import copy2, copystat
-from concurrent.futures import ThreadPoolExecutor
+from tqdm.contrib.concurrent import thread_map
 from argparse import ArgumentParser
-from hashlib import md5
+from imohash import hashfile
 import logging
 from exiftool import ExifToolHelper
 from datetime import datetime
 from tqdm import tqdm
+from shutil import move
 
 class MediaFileSorter:
-    def __init__(self, source_dir, dest_dir, num_workers, log_file):
+    def __init__(self, source_dir, dest_dir, log_file):
         self.source_dir = source_dir
         self.dest_dir = dest_dir
-        self.num_workers = num_workers
         self.log_file = log_file
 
         self.total_files_copied = 0
         self.files_copied_by_type = {}
+        
+        self.if_move = True
+        
+        self.dry_run = False
+        
+        self.no_date_folder = None
+        self.error_folder = None
 
     def create_destination_folders(self):
         makedirs(self.dest_dir, exist_ok=True)
-        no_date_folder = path.join(self.dest_dir, "00_no_date_found")
-        makedirs(no_date_folder, exist_ok=True)
+        self.no_date_folder = path.join(self.dest_dir, "00_no_date_found")
+        makedirs(self.no_date_folder, exist_ok=True)
         self.error_folder = path.join(self.dest_dir, "00_media_error")
         makedirs(self.error_folder, exist_ok=True)
-
+        
     def get_media_date(self, file):
         date = None
         try:
             with ExifToolHelper() as et:
                 output = et.get_metadata(file)[0]
-        except:
+                # print(output)
+        except Exception as e:
+            print(f"Error reading metadata: {e}")
             return None
+
+        date_tags = [
+            'QuickTime:CreateDate',
+            'QuickTime:CreationDate',
+            'EXIF:CreateDate',
+            'EXIF:DateTimeOriginal',
+            'File:ModificationDate',  # Skip because throws no date for .AAE files
+            'File:FileModifyDate',
+            # 'File:FileAccessDate',
+            # 'File:FileInodeChangeDate'
+        ]
         
-        if output.get('EXIF:CreateDate'):
-            date = output.get('EXIF:CreateDate')
-        elif output.get('QuickTime:CreationDate'):
-            date = output.get('QuickTime:CreationDate')
-            # if date is null:
-            if date.split(' ')[0] == '0000:00:00':
-                date = output.get('File:FileModifyDate')
-        elif output.get('File:FileModifyDate'):
-            date = output.get('File:FileModifyDate')
+        # AAE sidecar files have limited tags
+        if output['File:FileType'] == 'AAE':
+            date_tags = [date_tags[5]]
         
+        for tag in date_tags:
+            if tag in output:
+                date_str = output[tag]
+
+                try:
+                    date = date_str[:10].replace(":", "-")
+                    
+                    # if datae is all zeros then continue to the next tag
+                    # quicktime containers without a create date continues to ModificationDate tag
+                    if date == '0000-00-00':
+                        continue
+                except ValueError:
+                    continue
+                break
+
         if date:
+            # print(date)
+            # logging.info(f"File {file} moving to folder: {str(date)}")
             return date
         else:
             return None
@@ -51,68 +82,102 @@ class MediaFileSorter:
     def copy_file(self, file):
         try:
             date_taken = self.get_media_date(file)
-            if date_taken:
-                new_date = datetime.strptime(date_taken.split(' ')[0], "%Y:%m:%d")
-                date_taken = new_date.strftime("%Y-%m-%d")
-                dest_folder = path.join(self.dest_dir, date_taken)
-                makedirs(dest_folder, exist_ok=True)
 
+            if date_taken:
+                date_folder = date_taken
+                dest_folder = path.join(self.dest_dir, date_folder)
+                if not self.dry_run:
+                    makedirs(dest_folder, exist_ok=True)
+
+                current_folder = path.dirname(file)
                 base_name, ext = path.splitext(path.basename(file))
                 dest_file = path.join(dest_folder, path.basename(file))
 
+                # Check if the file is already in the correct folder
+                if current_folder == dest_folder:
+                    # The file is in the correct folder, no action needed
+                    return
+
+                # Handling duplicate file names
                 counter = 1
-                new_file_hash = md5(open(file, 'rb').read()).hexdigest()
-                # if dest_file does NOT exist, copy it, no loop
-                # if dest_file exists, compare hash to original file, if hashes are not equal increment filename, 
+                new_file_hash = hashfile(file, hexdigest=True)
                 while path.exists(dest_file):
-                    
-                    existing_file_hash = md5(open(dest_file, 'rb').read()).hexdigest()
+                    existing_file_hash = hashfile(dest_file, hexdigest=True)
 
                     if new_file_hash != existing_file_hash:
                         dest_file = path.join(dest_folder, f"{base_name}_{counter}{ext}")
                         counter += 1
                     else:
-                        # print(f"Skipping copy of {file} as it already exists with the same content.")
-                        break
-
-                copy2(file, dest_file)
-                copystat(file, dest_file)
+                        # File already exists with the same content
+                        logging.info(f"File already exists at dest folder: {date_taken} {file}")
+                        # remove source item since its saved already
+                        if self.if_move:
+                            remove(file)
+                        return
+                
+                if not self.dry_run:
+                    if self.if_move:
+                        move(file, dest_file)
+                    else:
+                        copy2(file, dest_file)
+                        copystat(file, dest_file)
+                    
                 self.total_files_copied += 1
-                if ext not in self.files_copied_by_type:
-                    self.files_copied_by_type[ext] = 1
-                else:
-                    self.files_copied_by_type[ext] += 1
+                ext = path.splitext(file)[1]
+                self.files_copied_by_type[ext] = self.files_copied_by_type.get(ext, 0) + 1
             else:
-                dest_file = path.join(self.no_date_folder, path.basename(file))
-                copy2(file, dest_file)
-                copystat(file, dest_file)
-                self.total_files_copied += 1
-                if 'no_date' not in self.files_copied_by_type:
-                    self.files_copied_by_type['no_date'] = 1
-                else:
-                    self.files_copied_by_type['no_date'] += 1
+                # Handle files with no date
+                self.handle_no_date_file(file)
         except Exception as e:
-            logging.error(f"Error processing {file}: {str(e)}")
-            dest_file = path.join(self.error_folder, path.basename(file))
-            copy2(file, dest_file)
-            copystat(file, dest_file)
-            self.total_files_copied += 1
-            if 'error' not in self.files_copied_by_type:
-                self.files_copied_by_type['error'] = 1
+            self.handle_error_file(file, e)
+
+    def handle_no_date_file(self, file):
+        dest_file = path.join(self.no_date_folder, path.basename(file))
+        if not self.dry_run:
+            if self.if_move:
+                move(file, dest_file)
             else:
-                self.files_copied_by_type['error'] += 1
+                copy2(file, dest_file)
+                copystat(file, dest_file)
+            
+        self.total_files_copied += 1
+        self.files_copied_by_type['no_date'] = self.files_copied_by_type.get('no_date', 0) + 1
         
-            
+        logging.info(f'NO DATE FILE: {file}')
 
+    def handle_error_file(self, file, error):
+        
+        dest_file = path.join(self.error_folder, path.basename(file))
+        if not self.dry_run:
+            if self.if_move:
+                move(file, dest_file)
+            else:
+                copy2(file, dest_file)
+                copystat(file, dest_file)
+                
+        self.total_files_copied += 1
+        self.files_copied_by_type['error'] = self.files_copied_by_type.get('error', 0) + 1
+        
+        logging.error(f"Error processing {file}: {str(error)}")
+        
     def sort_media_files(self):
+        
         self.create_destination_folders()
+        
+        # List of common video file extensions (add or remove as needed)
+        # video_extensions = {'.mp4', '.mov', '.aae'}
+        
+        # media_files = [path.join(root, filename)
+        #                    for root, _, files in walk(self.source_dir)
+        #                    for filename in files
+        #                    if path.splitext(filename)[1].lower() in video_extensions]
+        
+        media_files = [path.join(root, filename) for root, _, files in walk(self.source_dir) for filename in files if not filename.startswith(".")]
+        
+        print(f'There are {len(media_files)} files to look through!')
+        # print(media_files)
 
-        with ThreadPoolExecutor(max_workers=self.num_workers) as executor:
-            media_files = [path.join(root, filename) for root, _, files in walk(self.source_dir) for filename in files]
-            
-            # Using tqdm to wrap around the executor.map function to display a progress bar
-            for _ in tqdm(executor.map(self.copy_file, media_files), total=len(media_files)):
-                pass
+        thread_map(self.copy_file, media_files)
 
         with open(self.log_file, 'w') as log:
             log.write(f"Total Files Copied: {self.total_files_copied}\n")
@@ -124,12 +189,11 @@ def main():
     parser = ArgumentParser(description="Sort media files based on date taken.")
     parser.add_argument("source_dir", help="Source directory containing media files.")
     parser.add_argument("dest_dir", help="Destination directory for sorted files.")
-    parser.add_argument("num_threads", type=int, help="Number of threads for concurrent processing")
     parser.add_argument("log_file", help="Output log file")
 
     args = parser.parse_args()
 
-    sorter = MediaFileSorter(args.source_dir, args.dest_dir, args.num_threads, args.log_file)
+    sorter = MediaFileSorter(args.source_dir, args.dest_dir, args.log_file)
     sorter.sort_media_files()
 
 if __name__ == "__main__":
@@ -140,3 +204,6 @@ if __name__ == "__main__":
 # command python sort_img.py /mnt/nas/unsorted_photos_to_import/import_pictures_9MAR-2/ /mnt/nas/unsorted_photos_to_import/TEST_SORT_OUTPUT/ 45  /mnt/nas/unsorted_photos_to_import/test_sort_output.txt
 # command python sort_img.py '/mnt/nas/unsorted_photos_to_import/DJI AIR Drone Dragon 13SEP20/' /mnt/nas/unsorted_photos_to_import/TEST_SORT_OUTPUT/ 45  /mnt/nas/unsorted_photos_to_import/test_sort_output-dji.txt
 # time python sort_img.py /mnt/nas/unsorted_photos_to_import/ /mnt/nas/TEST_SORT_OUTPUT/ 47 /mnt/nas/test_sort_output.txt
+
+# time python sort_img3.py /mnt/nas/00_IMPORT_PICTURES_00/ /mnt/nas/Pictures/ ./test_sort_output_final_03.txt
+# time python sort_img3.py ./test/ ./test_output/ ./test_sort_output1.txt
